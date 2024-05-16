644b46b9f6e57f7e27e73e64c4904abf7f1de699
CatPope <dajung2140@gmail.com>
2024-05-16 20:02:18 +0900

    24.05.16

---
+++ GagnamAI/code/ardoino/Siren.py
@@ -0,0 +1,26 @@
+from jikko.jikko import *
+
+jk = Pyjikko()
+
+PORT = "COM4"
+
+jk.serial_connect(PORT)
+jk.start()
+
+LED = 5
+BUZZER = 6
+
+
+
+while (1):
+    jk.led_digital(LED, HIGH)
+    
+    jk.buzzer(BUZZER, 131, 1)
+    jk.buzzer(BUZZER, 147, 1)
+    jk.buzzer(BUZZER, 165, 1)
+    jk.buzzer(BUZZER, 175, 1)
+    jk.buzzer(BUZZER, 196, 1)
+
+    jk.led_digital(LED, LOW)
+    
+    time.sleep(3)
---
+++ GagnamAI/code/python/행운의숫자.py
@@ -0,0 +1,5 @@
+a = int(input())
+
+for i in range(1, a+1):
+    for j in range(1, 10):
+        print(i, "*", j, "=", i*j)
